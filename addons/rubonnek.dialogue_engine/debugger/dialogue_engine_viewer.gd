faea6a872afb552358823632bec529635f072395
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2024-12-18 12:56:56 -0500

    Use @export instead of find_child on the debugger

--- addons/rubonnek.dialogue_engine/debugger/dialogue_engine_viewer.gd
+++ addons/rubonnek.dialogue_engine/debugger/dialogue_engine_viewer.gd
@@ -1,32 +1,33 @@
 @tool
 extends PanelContainer
 
-@onready var m_dialogue_engine_viewer_input_blocker_center_container : CenterContainer = find_child("DialogueEngineViewerInputBlockerCenterContainer")
-@onready var m_dialogue_engine_viewer_input_blocker_hint_rich_text_label : RichTextLabel = find_child("DialogueEngineViewerInputBlockerHintRichTextLabel")
-@onready var m_default_input_blocker_hint_text : String = m_dialogue_engine_viewer_input_blocker_hint_rich_text_label.get_text()
+@export var dialogue_engine_viewer_input_blocker_center_container_ : CenterContainer
+@export var dialogue_engine_viewer_input_blocker_hint_rich_text_label_ : RichTextLabel
 
-@onready var m_dialogue_engine_viewer_engine_selection_line_edit : LineEdit  = find_child("DialogueEngineViewerEngineSelectionLineEdit")
-@onready var m_dialogue_engine_viewer_engine_selection_tree : Tree  = find_child("DialogueEngineViewerEngineSelectionTree")
-@onready var m_dialogue_engine_viewer_graph_edit : GraphEdit = find_child("DialogueEngineViewerGraphEdit")
+@export var dialogue_engine_viewer_engine_selection_line_edit_ : LineEdit
+@export var dialogue_engine_viewer_engine_selection_tree_ : Tree
+@export var dialogue_engine_viewer_graph_edit_ : GraphEdit
+
+@onready var m_default_input_blocker_hint_text : String = dialogue_engine_viewer_input_blocker_hint_rich_text_label_.get_text()
 var m_base_dialogue_entry_graph_node_packed_scene : PackedScene = preload("base_dialogue_entry_graph_node.tscn")
 
 var _m_remote_dialogue_engine_id_to_tree_item_map_cache : Dictionary
 
 func _ready() -> void:
 	# Create the root tree item -- we'll ignore it by default
-	var _root : TreeItem = m_dialogue_engine_viewer_engine_selection_tree.create_item()
+	var _root : TreeItem = dialogue_engine_viewer_engine_selection_tree_.create_item()
 
 	# Connect Tree Signals
-	var _success : int = m_dialogue_engine_viewer_engine_selection_tree.item_selected.connect(__on_tree_item_selected)
-	_success = m_dialogue_engine_viewer_engine_selection_tree.nothing_selected.connect(__on_tree_nothing_selected)
+	var _success : int = dialogue_engine_viewer_engine_selection_tree_.item_selected.connect(__on_tree_item_selected)
+	_success = dialogue_engine_viewer_engine_selection_tree_.nothing_selected.connect(__on_tree_nothing_selected)
 
 	# Connect line edit for filtering the DialogueEngines list
-	_success = m_dialogue_engine_viewer_engine_selection_line_edit.text_changed.connect(__on_dialogue_selection_line_edit_text_changed)
+	_success = dialogue_engine_viewer_engine_selection_line_edit_.text_changed.connect(__on_dialogue_selection_line_edit_text_changed)
 
 
 func __on_dialogue_selection_line_edit_text_changed(p_filter : String) -> void:
 	# Hide the TreeItem that don't match the filter
-	var root : TreeItem = m_dialogue_engine_viewer_engine_selection_tree.get_root()
+	var root : TreeItem = dialogue_engine_viewer_engine_selection_tree_.get_root()
 	var column : int = 0
 	for child : TreeItem in root.get_children():
 		if p_filter.is_empty() or p_filter in child.get_text(column):
@@ -35,11 +36,11 @@ func __on_dialogue_selection_line_edit_text_changed(p_filter : String) -> void:
 			child.set_visible(false)
 
 	# Select an item (if any):
-	m_dialogue_engine_viewer_engine_selection_tree.deselect_all()
+	dialogue_engine_viewer_engine_selection_tree_.deselect_all()
 	var did_select_item : bool = false
 	for child : TreeItem in root.get_children():
 		if child.is_visible():
-			m_dialogue_engine_viewer_engine_selection_tree.set_selected(child, column) # emits item_selected signal
+			dialogue_engine_viewer_engine_selection_tree_.set_selected(child, column) # emits item_selected signal
 			child.select(column) # highlights the item on the Tree
 			did_select_item = true
 			break
@@ -68,7 +69,7 @@ func on_editor_debugger_plugin_capture(p_message : String, p_data : Array) -> bo
 			target_name = target_name + ":" + String.num_uint64(dialogue_engine_id)
 
 			# Create the associated tree_item and add it as metadata against the tree itself so that we can extract it easily when we receive messages from this specific DialogueEngine instance id
-			var dialogue_engine_tree_item : TreeItem = m_dialogue_engine_viewer_engine_selection_tree.create_item()
+			var dialogue_engine_tree_item : TreeItem = dialogue_engine_viewer_engine_selection_tree_.create_item()
 			dialogue_engine_tree_item.set_text(column, target_name)
 			_m_remote_dialogue_engine_id_to_tree_item_map_cache[dialogue_engine_id] = dialogue_engine_tree_item
 
@@ -170,31 +171,31 @@ func __on_session_started() -> void:
 	_m_remote_dialogue_engine_id_to_tree_item_map_cache.clear()
 
 	# Clear the dialogue engine tree
-	m_dialogue_engine_viewer_engine_selection_tree.clear()
-	var _root : TreeItem = m_dialogue_engine_viewer_engine_selection_tree.create_item() # need to recreate the root TreeItem which gets ignored
+	dialogue_engine_viewer_engine_selection_tree_.clear()
+	var _root : TreeItem = dialogue_engine_viewer_engine_selection_tree_.create_item() # need to recreate the root TreeItem which gets ignored
 
 	# Clean up the graph edit
 	__cleanup_graph_edit()
 
 	# Show hint
-	m_dialogue_engine_viewer_input_blocker_hint_rich_text_label.set_text(_m_choose_dialogue_string)
-	m_dialogue_engine_viewer_input_blocker_center_container.show()
+	dialogue_engine_viewer_input_blocker_hint_rich_text_label_.set_text(_m_choose_dialogue_string)
+	dialogue_engine_viewer_input_blocker_center_container_.show()
 
 
 func __on_tree_item_selected() -> void:
 	# Only update the GraphEdit if the selected tree item differs from the previously selected one
-	var selected_tree_item : TreeItem = m_dialogue_engine_viewer_engine_selection_tree.get_selected()
+	var selected_tree_item : TreeItem = dialogue_engine_viewer_engine_selection_tree_.get_selected()
 	if is_instance_valid(selected_tree_item):
-		if m_dialogue_engine_viewer_input_blocker_center_container.is_visible():
-			m_dialogue_engine_viewer_input_blocker_center_container.hide()
+		if dialogue_engine_viewer_input_blocker_center_container_.is_visible():
+			dialogue_engine_viewer_input_blocker_center_container_.hide()
 		__refresh_graph_for_currently_selected_item()
 
 
 func __on_tree_nothing_selected() -> void:
 	__cleanup_graph_edit()
-	m_dialogue_engine_viewer_input_blocker_hint_rich_text_label.set_text(_m_choose_dialogue_string)
-	m_dialogue_engine_viewer_input_blocker_center_container.show()
-	m_dialogue_engine_viewer_engine_selection_tree.deselect_all()
+	dialogue_engine_viewer_input_blocker_hint_rich_text_label_.set_text(_m_choose_dialogue_string)
+	dialogue_engine_viewer_input_blocker_center_container_.show()
+	dialogue_engine_viewer_engine_selection_tree_.deselect_all()
 
 
 func __update_graph_if_needed(p_dialogue_engine_id : int) -> void:
@@ -203,7 +204,7 @@ func __update_graph_if_needed(p_dialogue_engine_id : int) -> void:
 
 
 func __is_selected_tree_item_related_to_dialogue_engine(p_dialogue_engine_id : int) -> bool:
-	var selected_tree_item : TreeItem = m_dialogue_engine_viewer_engine_selection_tree.get_selected()
+	var selected_tree_item : TreeItem = dialogue_engine_viewer_engine_selection_tree_.get_selected()
 	if is_instance_valid(selected_tree_item):
 		var target_dialogue_engine_tree_item : TreeItem = _m_remote_dialogue_engine_id_to_tree_item_map_cache[p_dialogue_engine_id]
 		if target_dialogue_engine_tree_item == selected_tree_item:
@@ -222,7 +223,7 @@ func __update_graph_node_highlights_if_needed(p_dialogue_engine : DialogueEngine
 		# Highlight all the previosuly visited nodes in a specific color
 		for index : int in dialogue_entries_visited.size() - 1:
 			var graph_node_visited : String = dialogue_entries_visited[index]
-			var graph_node : GraphNode = m_dialogue_engine_viewer_graph_edit.get_node_or_null(NodePath(graph_node_visited))
+			var graph_node : GraphNode = dialogue_engine_viewer_graph_edit_.get_node_or_null(NodePath(graph_node_visited))
 			if is_instance_valid(graph_node):
 				var style_box_flat : StyleBoxFlat = graph_node.get_theme_stylebox(&"titlebar").duplicate(true)
 				style_box_flat.set_bg_color(_m_previously_visited_color)
@@ -232,7 +233,7 @@ func __update_graph_node_highlights_if_needed(p_dialogue_engine : DialogueEngine
 		# Highlight the last node visited as a different color to identify it from the rest
 		if not dialogue_entries_visited.is_empty():
 			var graph_node_visited : String = dialogue_entries_visited[-1]
-			var graph_node : GraphNode = m_dialogue_engine_viewer_graph_edit.get_node_or_null(NodePath(graph_node_visited))
+			var graph_node : GraphNode = dialogue_engine_viewer_graph_edit_.get_node_or_null(NodePath(graph_node_visited))
 			if is_instance_valid(graph_node):
 				var style_box_flat : StyleBoxFlat = graph_node.get_theme_stylebox(&"titlebar").duplicate(true)
 				if p_dialogue_engine.has_meta(&"dialogue_finished"):
@@ -247,7 +248,7 @@ func __update_graph_node_highlights_if_needed(p_dialogue_engine : DialogueEngine
 
 func __clear_graph_node_highlights_if_needed(p_dialogue_engine_id : int) -> void:
 	if __is_selected_tree_item_related_to_dialogue_engine(p_dialogue_engine_id):
-		for child : Node in m_dialogue_engine_viewer_graph_edit.get_children():
+		for child : Node in dialogue_engine_viewer_graph_edit_.get_children():
 			if child is GraphNode:
 				var graph_node : GraphNode = child
 				if graph_node.has_theme_stylebox_override(&"titlebar"):
@@ -257,7 +258,7 @@ func __clear_graph_node_highlights_if_needed(p_dialogue_engine_id : int) -> void
 
 
 func __refresh_graph_for_currently_selected_item() -> void:
-	var selected_tree_item : TreeItem = m_dialogue_engine_viewer_engine_selection_tree.get_selected()
+	var selected_tree_item : TreeItem = dialogue_engine_viewer_engine_selection_tree_.get_selected()
 	if not is_instance_valid(selected_tree_item):
 		push_warning("DialogueEngineViewer: Selected tree item is invalid! This should not happen! Unable to update DialogueEngine visualization!" )
 		return
@@ -374,7 +375,7 @@ func __refresh_graph_for_currently_selected_item() -> void:
 			metadata_h_split_container.free()
 
 		graph_node.set_slot_enabled_left(0, true) # this slot is always open since any DialogueEntry can connect to any DialogueEntry
-		m_dialogue_engine_viewer_graph_edit.add_child(graph_node)
+		dialogue_engine_viewer_graph_edit_.add_child(graph_node)
 
 	# Process GraphNode connections
 	var node_string_ids : Array = []
@@ -391,7 +392,7 @@ func __refresh_graph_for_currently_selected_item() -> void:
 			var from_port_in_connection_index_form : int = 0
 			var to_node : String = goto_dialogue_entry.get_id_as_text()
 			var to_port : int = 0
-			var _success : int = m_dialogue_engine_viewer_graph_edit.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
+			var _success : int = dialogue_engine_viewer_graph_edit_.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
 		elif dialogue_entry.has_condition():
 			var goto_ids : Dictionary = dialogue_entry.get_condition_goto_ids()
 
@@ -403,7 +404,7 @@ func __refresh_graph_for_currently_selected_item() -> void:
 				var from_port_in_connection_index_form : int = 0
 				var to_node : String = goto_dialogue_entry.get_id_as_text()
 				var to_port : int = 0
-				var _success : int = m_dialogue_engine_viewer_graph_edit.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
+				var _success : int = dialogue_engine_viewer_graph_edit_.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
 
 			# Add false connection
 			var false_goto_id : int = goto_ids[false]
@@ -413,7 +414,7 @@ func __refresh_graph_for_currently_selected_item() -> void:
 				var from_port_in_connection_index_form : int = 1
 				var to_node : String = goto_dialogue_entry.get_id_as_text()
 				var to_port : int = 0
-				var _success : int = m_dialogue_engine_viewer_graph_edit.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
+				var _success : int = dialogue_engine_viewer_graph_edit_.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
 		elif not dialogue_entry.has_options():
 			# Find the next available goto entry that has the same branch ID -- i.e. find the default goto if any
 			var current_branch_id : int = dialogue_entry.get_branch_id()
@@ -425,7 +426,7 @@ func __refresh_graph_for_currently_selected_item() -> void:
 					break
 			if is_instance_valid(goto_dialogue_entry):
 				# Enable slot
-				var dialogue_entry_graph_node : GraphNode = m_dialogue_engine_viewer_graph_edit.get_node(NodePath(dialogue_entry_id_as_string))
+				var dialogue_entry_graph_node : GraphNode = dialogue_engine_viewer_graph_edit_.get_node(NodePath(dialogue_entry_id_as_string))
 				var from_port_in_connection_index_form : int = 0
 				dialogue_entry_graph_node.set_slot_enabled_right(from_port_in_connection_index_form, true)
 
@@ -433,7 +434,7 @@ func __refresh_graph_for_currently_selected_item() -> void:
 				var from_node : String = dialogue_entry_id_as_string
 				var to_node : String = goto_dialogue_entry.get_id_as_text()
 				var to_port : int = 0
-				var _success : int = m_dialogue_engine_viewer_graph_edit.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
+				var _success : int = dialogue_engine_viewer_graph_edit_.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
 		elif dialogue_entry.has_options():
 			var outgoing_port_number : int = 0
 			for option_id : int in dialogue_entry.get_option_count():
@@ -445,12 +446,12 @@ func __refresh_graph_for_currently_selected_item() -> void:
 					var from_port_in_connection_index_form : int = outgoing_port_number
 					var to_node : String = goto_dialogue_entry.get_id_as_text()
 					var to_port : int = 0
-					var _success : int = m_dialogue_engine_viewer_graph_edit.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
+					var _success : int = dialogue_engine_viewer_graph_edit_.connect_node(from_node, from_port_in_connection_index_form, to_node, to_port)
 					outgoing_port_number += 1
 
 
 	# Disable the first slot of all the nodes that have no incoming connections to make the graph more readable (this is purely for aesthetics)
-	var connections_list : Array = m_dialogue_engine_viewer_graph_edit.get_connection_list()
+	var connections_list : Array = dialogue_engine_viewer_graph_edit_.get_connection_list()
 	for target_node_name : String in node_string_ids:
 		var found_incoming_connection_to_first_slot : bool = false
 		for connection_entry : Dictionary in connections_list:
@@ -462,22 +463,22 @@ func __refresh_graph_for_currently_selected_item() -> void:
 				found_incoming_connection_to_first_slot = true
 				break
 		if not found_incoming_connection_to_first_slot:
-			var graph_node : GraphNode = m_dialogue_engine_viewer_graph_edit.get_node(NodePath(target_node_name))
+			var graph_node : GraphNode = dialogue_engine_viewer_graph_edit_.get_node(NodePath(target_node_name))
 			graph_node.set_slot_enabled_left(0, false)
 
 	# Do not block input against the GraphEdit
-	m_dialogue_engine_viewer_input_blocker_center_container.hide()
+	dialogue_engine_viewer_input_blocker_center_container_.hide()
 
 	# Highlight the nodes
 	__update_graph_node_highlights_if_needed(dialogue_engine)
 
 	# Finally auto arrange the nodes
-	m_dialogue_engine_viewer_graph_edit.arrange_nodes()
+	dialogue_engine_viewer_graph_edit_.arrange_nodes()
 
 
 func __cleanup_graph_edit() -> void:
-	m_dialogue_engine_viewer_graph_edit.clear_connections()
-	for child_node : Node in m_dialogue_engine_viewer_graph_edit.get_children():
+	dialogue_engine_viewer_graph_edit_.clear_connections()
+	for child_node : Node in dialogue_engine_viewer_graph_edit_.get_children():
 		if child_node is GraphNode:
 			child_node.free()
 # ===== VISUALIZATION FUNCTIONS END ====
